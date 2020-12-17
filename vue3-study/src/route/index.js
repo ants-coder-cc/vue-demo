92cfcb92de23285b9221646af8481b937b09b462
“ <824019782@qq.com>
2020-12-17 14:31:58 +0800

    Vue router

---
+++ vue3-study/src/route/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+const home = () => import("../views/home/index");
+const login = () => import("../views/login/index");
+
+const routes = [
+	{ path: "/", redirect: "/home" },
+	{
+		path: "/home",
+		name: "home",
+		component: home,
+	},
+	{
+		path: "/login",
+		name: "login",
+		component: login,
+	},
+];
+export const router = createRouter({
+	history: createWebHashHistory(),
+	routes: routes,
+});
---
+++ vue3-study/src/views/count/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="app-container"></div>
+</template>
+
+<script>
+export default {
+	components: {},
+	data() {
+		return {};
+	},
+	methods: {},
+	created() {},
+	mounted() {},
+};
+</script>
+<style lang="scss" scoped></style>
